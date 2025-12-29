98d344113cc2fd32cbafa4d73e2ffedc65735b67
frisbee09 <5574633+frisbee09@users.noreply.github.com>
2025-12-29 21:30:07 +0000

    It's inefficient but it works

--- src/8/solution_two.py
+++ src/8/solution_two.py
@@ -26,7 +26,7 @@ class Junction:
         self.id = str(uuid4())[:6]
         self.nearest_neighbour_id = None
         self.nearest_neighbour_distance = None
-        self.circuit = None
+        self.circuit = self.id
 
         self.x = x
         self.y = y
@@ -52,7 +52,6 @@ def main():
     line_generator = read_input(Path(__file__).parent / "input.txt")
     # line_generator = read_input(Path(__file__).parent / "test_input.txt")
     j_boxes_by_id: dict[str, Junction] = {}
-    circuits: dict[str, set[Junction]] = {}
 
     logger.info("Reading coords and constructing circuits")
 
@@ -61,30 +60,32 @@ def main():
         j = Junction(x, y, z)
         j_boxes_by_id[j.id] = j
 
+    circuits: dict[str, set[Junction]] = {
+        j.circuit: set([j.id]) for j in j_boxes_by_id.values()
+    }
     logger.info("Connecting the closest ten...")
     total_circuits = len(j_boxes_by_id.keys())
     connections_made = 0
 
     all_j_boxes = list(j_boxes_by_id.values())
-    all_conns = sorted(
-        [
-            {"distance": i.distance_from(j), "from": i, "to": j}
-            for i_idx, i in enumerate(all_j_boxes)
-            for j in all_j_boxes[i_idx + 1 :]
-        ],
-        key=(lambda j: j["distance"]),
-    )
+    all_conns = [
+        x
+        for x in sorted(
+            [
+                {"distance": i.distance_from(j), "from": i, "to": j}
+                for i in all_j_boxes
+                for j in all_j_boxes
+                if i.id != j.id
+            ],
+            key=(lambda j: j["distance"]),
+        )
+    ]
 
-    for conn in all_conns[:1000]:
+    for conn in all_conns:
         j = conn["from"]
         nnb = conn["to"]
 
-        if j.circuit is None and nnb.circuit is None:
-            # Both are un-attached. We create a new circuit
-            circuit_id_to_join = str(uuid4())[:6]
-            circuits[circuit_id_to_join] = set([j.id, nnb.id])
-
-        elif j.circuit == nnb.circuit:
+        if j.circuit == nnb.circuit:
             # Both are attached to each other already, skip
             continue
 
@@ -93,30 +94,47 @@ def main():
             circuit_id_to_join = j.circuit or nnb.circuit
             circuits[circuit_id_to_join].add(j.id)
             circuits[circuit_id_to_join].add(nnb.id)
+            j.circuit = circuit_id_to_join
+            nnb.circuit = circuit_id_to_join
 
         else:
             # Two distinct circuits combine!
-            circuit_id_to_join = j.circuit
+            circuits_by_size = sorted(
+                [j.circuit, nnb.circuit],
+                key=(lambda id: len(circuits.get(id))),
+                reverse=True,
+            )
 
             # Grab the circuits
-            circuit_to_join = circuits[j.circuit]
-            circuit_to_merge = circuits[nnb.circuit]
+            circuit_to_join = circuits[circuits_by_size[0]]
+            circuit_to_merge = circuits[circuits_by_size[1]]
 
             # Union the sets
-            circuits[j.circuit] = circuit_to_join.union(circuit_to_merge)
+            circuits[circuits_by_size[0]] = circuit_to_join.union(circuit_to_merge)
 
             # Update the Jbox states to reflect the merge
             for m in circuit_to_merge:
-                j_boxes_by_id[m].circuit = j.circuit
+                j_boxes_by_id[m].circuit = circuits_by_size[0]
 
-        j.circuit = circuit_id_to_join
-        nnb.circuit = circuit_id_to_join
+            del circuits[circuits_by_size[1]]
+
         total_circuits -= 1
         connections_made += 1
 
-    logger.info(circuits)
-    circuits_by_length = sorted([len(x) for x in circuits.values()], reverse=True)[:3]
-    answer = reduce(operator.mul, circuits_by_length)
+        not_connected_jbs = reduce(
+            operator.add, sorted([len(x) for x in circuits.values()], reverse=True)[1:]
+        )
+        if not_connected_jbs <= 1:
+            break
+
+    # Get the last remaining node
+    last_node_id = next(list(j) for j in circuits.values() if len(j) == 1)[0]
+    node = j_boxes_by_id[last_node_id]
+    node.get_nearest_unconnected_neighbour(j_boxes_by_id.values())
+    x1 = node.x
+    x2 = j_boxes_by_id[node.nearest_neighbour_id].x
+    answer = x1 * x2
+
     # assert answer == 40
     logger.info(answer)
 
